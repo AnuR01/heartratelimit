e7b284e4bfe9b2819cfa45a356f9b7ab760686a3
AnuR01 <raw.anu@outlook.com>
2022-09-14 16:12:23 +0530

    This is my first commit for Heart Rate Limit .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+
+
+  const [age, setAge]= useState(0)
+  const [hrl,set]= useState(0)
+  
+
+  
+  function calculate(){
+    const uHrl=(220-age)* 0.85;
+    const lHrl=(220-age)* 0.65;
+    setHrl(lHrl + "-" + uHrl)
+  }
+  
+  return(
+    <form div id='content'>
+      <div>
+      <h3>Heart Rate Limits</h3>
+      </div>
+      <div>
+      <label>Age</label> &nbsp;
+      <input type="number" value={age} onChange={a=> setAge(a.target.value)}></input>
+      </div>
+      <br></br>
+      <div>
+      <label>Heart Rate Limits</label> &nbsp;
+      <button>{hrl}</button>
+      </div>
+      <br></br>
+      <div>
+      <button type="button" class="btn btn-primary"onClick={calculate}>Calculate</button>
+      </div>
+    </form>
   );
 }
 
